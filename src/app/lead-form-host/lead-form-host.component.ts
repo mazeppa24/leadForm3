e126fc38550e8c1fc0407ae94e95d776394ebd22
ari <messageq@nym.hush.com>
2025-03-07 08:58:53 +0100

    proper grid layout

--- src/app/lead-form-host/lead-form-host.component.ts
+++ src/app/lead-form-host/lead-form-host.component.ts
@@ -1,7 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, OnInit, inject, signal, computed, effect } from '@angular/core';
+import { NxColComponent, NxLayoutComponent, NxRowComponent } from "@aposin/ng-aquila/grid";
 import { LeadFormComponent } from "../lead-form/lead-form.component";
-import {ProfileImageComponent} from "../profile-image/profile-image.component";
-import {NxColComponent, NxLayoutComponent, NxRowComponent} from "@aposin/ng-aquila/grid";
+import { ProfileImageComponent } from "../profile-image/profile-image.component";
+import { InitialAppParamsService } from "../core/services/initial-app-params/initial-app-params.service";
+import { ImageProfileService, ImageProfileResult } from "../core/services/profile-image/image-profile.service";
+
 
 @Component({
   selector: "app-lead-form-host",
@@ -10,4 +13,29 @@ import {NxColComponent, NxLayoutComponent, NxRowComponent} from "@aposin/ng-aqui
   templateUrl: "./lead-form-host.component.html",
   styleUrl: "./lead-form-host.component.scss",
 })
-export class LeadFormHostComponent {}
+export class LeadFormHostComponent implements OnInit {
+
+  private initialAppParamsService = inject(InitialAppParamsService);
+  private imageProfileService = inject(ImageProfileService);
+
+
+  decodedEmail = computed(
+    () =>
+      this.initialAppParamsService.initialAppParams()?.preselectedEmail || "",
+  );
+
+  protected profileImageData = signal(new ImageProfileResult());
+
+  constructor() {
+    effect(async () => {
+      if (this.decodedEmail()) {
+        console.log("effect decodedEmail", this.decodedEmail());
+        this.profileImageData.set(await this.imageProfileService.fetchKbImageLink(this.decodedEmail()))
+      }
+    });
+  }
+  
+  ngOnInit(): void {
+    console.log("ngOnInit");
+  }
+}
